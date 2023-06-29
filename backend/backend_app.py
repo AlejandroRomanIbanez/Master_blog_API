54e2da1f07d233f3bb20e735b3f375b64ac09efc
Alejandro Roman Ibañez <roman.ibanez.alex@gmail.com>
2023-06-29 10:53:53 +0200

    Added backend, frontend and post and users json, a new update.html form

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,18 +1,278 @@
-from flask import Flask, jsonify
+import json
+from flask import Flask, jsonify, request, session
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
-CORS(app)  # This will enable CORS for all routes
+app.secret_key = "pass1"
+CORS(app)
+app.config['JWT_SECRET_KEY'] = 'pass'
+jwt = JWTManager(app)
+limiter = Limiter(app)
 
-POSTS = [
-    {"id": 1, "title": "First post", "content": "This is the first post."},
-    {"id": 2, "title": "Second post", "content": "This is the second post."},
-]
+POSTS_FILE = "../backend/posts.json"
+USERS_FILE = "../backend/users.json"
+
+
+def open_files(file_data):
+    try:
+        with open(file_data, "r") as fileobj:
+            data = json.load(fileobj)
+    except (FileNotFoundError, json.JSONDecodeError):
+        data = []
+    return data
+
+
+def save_files(file_data, data):
+    with open(file_data, "w") as fileobj:
+        json.dump(data, fileobj, indent=4)
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return jsonify({"error": "Post was not found"}), 404
+
+
+@app.errorhandler(400)
+def bad_request_error(error):
+    return jsonify({"error": "Bad Request"}), 400
+
+
+def generate_id(posts):
+    new_id = max(post['id'] for post in posts) + 1
+    return new_id
+
+
+def sort_list(posts, sort, direction):
+    reverse = (direction == 'desc')
+    sorted_posts = sorted(posts, key=lambda post: post.get(sort), reverse=reverse)
+    return [post for post in sorted_posts]
+
+
+@app.route('/api/register', methods=['POST'])
+def register():
+    users = open_files(USERS_FILE)
+    new_user = request.get_json()
+
+    if 'username' not in new_user or 'password' not in new_user:
+        return jsonify({"error": "Missing field: username or password"}), 400
+
+    existing_user = next((user for user in users if user['username'] == new_user['username']), None)
+    if existing_user:
+        return jsonify({"error": "Username already exists"}), 400
+
+    if not users:
+        new_user['id'] = 1
+    else:
+        new_user['id'] = max(user['id'] for user in users) + 1
+
+    users.append(new_user)
+    save_files(USERS_FILE, users)
+    return jsonify({"message": "User registered successfully"}), 201
+
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    users = open_files(USERS_FILE)
+    credentials = request.get_json()
+    if 'username' in credentials and 'password' in credentials:
+        user = next((user for user in users if user['username'] == credentials['username']), None)
+        if user and user['password'] == credentials['password']:
+            session['user_id'] = user['id']
+            access_token = create_access_token(identity=user['id'])
+            return jsonify({"access_token": access_token}), 200
+    return jsonify({"error": "Invalid username or password"}), 401
 
 
 @app.route('/api/posts', methods=['GET'])
+@limiter.limit("10/minute")
 def get_posts():
-    return jsonify(POSTS)
+    sort = request.args.get('sort')
+    direction = request.args.get('direction', 'asc')
+
+    if sort is None:
+        return jsonify(open_files(POSTS_FILE))
+    if sort not in ['title', 'content', 'author', 'date'] or direction not in ['asc', 'desc']:
+        return jsonify({"error": "Invalid sort or direction value"}), 400
+
+    sorted_post = sort_list(open_files(POSTS_FILE), sort, direction)
+    page = int(request.args.get('page', 1))
+    limit = int(request.args.get('limit', 10))
+
+    start_index = (page - 1) * limit
+    end_index = start_index + limit
+
+    paginated_posts = list(sorted_post)[start_index:end_index]
+    return jsonify(paginated_posts)
+
+
+@app.route('/api/posts', methods=['POST'])
+@jwt_required()
+def add_post():
+    new_post = request.get_json()
+    if 'content' in new_post and 'title' in new_post:
+        posts = open_files(POSTS_FILE)
+        new_post['id'] = generate_id(posts)
+        posts.append(new_post)
+        save_files(POSTS_FILE, posts)
+        return jsonify(new_post), 201
+    else:
+        error_messages = []
+        if 'title' not in new_post:
+            error_messages.append("Missing field: title")
+        if 'content' not in new_post:
+            error_messages.append("Missing field: content")
+        if 'author' not in new_post:
+            error_messages.append("Missing field: author")
+        if 'date' not in new_post:
+            error_messages.append("Missing field: date")
+        error_message = {"error": ", ".join(error_messages)}
+        return jsonify(error_message), 400
+
+
+def find_post_by_id(post_id):
+    posts = open_files(POSTS_FILE)
+    for post in posts:
+        if post['id'] == post_id:
+            return post
+
+
+@app.route('/api/posts/<int:id>', methods=['DELETE'])
+@jwt_required()
+def delete_post(id):
+    posts = open_files(POSTS_FILE)
+    post = find_post_by_id(id)
+    if post is None:
+        return jsonify({"error": "Post was not found"}), 404
+    posts.remove(post)
+    save_files(POSTS_FILE, posts)
+    return {
+        "message": f"Post with id {id} has been deleted successfully."
+    }, 200
+
+
+@app.route('/api/posts/<int:id>', methods=['PUT'])
+@jwt_required()
+def update_post(id):
+    posts = open_files(POSTS_FILE)
+    post = find_post_by_id(id)
+    if post is None:
+        return jsonify({"error": "Post was not found"}), 404
+    updated_post = request.get_json()
+    if 'title' in updated_post:
+        post['title'] = updated_post['title']
+    if 'content' in updated_post:
+        post['content'] = updated_post['content']
+    if 'author' in updated_post:
+        post['author'] = updated_post['author']
+    if 'date' in updated_post:
+        post['date'] = updated_post['date']
+    save_files(POSTS_FILE, posts)
+    return jsonify(post), 200
+
+
+@app.route('/api/posts/search', methods=['GET'])
+def search_post():
+    all_post_find = []
+    posts = open_files(POSTS_FILE)
+    content = request.args.get('content')
+    title = request.args.get('title')
+    author = request.args.get('author')
+    date = request.args.get('date')
+    for post in posts:
+        if title and title.lower() in post['title'].lower():
+            all_post_find.append(post)
+        elif content and content.lower() in post['content'].lower():
+            all_post_find.append(post)
+        elif author and author.lower() in post['author'].lower():
+            all_post_find.append(post)
+        elif date and date.lower() in post['date'].lower():
+            all_post_find.append(post)
+    return jsonify(all_post_find)
+
+
+@app.route('/api/posts/<int:id>/comments', methods=['POST'])
+@jwt_required()
+def add_comment(id):
+    posts = open_files(POSTS_FILE)
+    post = find_post_by_id(id)
+    if post is None:
+        return jsonify({"error": "Post was not found"}), 404
+    new_comment = request.get_json()
+    if 'content' in new_comment and 'author' in new_comment:
+        new_comment_id = generate_id(post['comments'])
+        new_comment['id'] = new_comment_id
+        new_comment['author'] = new_comment['author']
+        new_comment['date'] = new_comment['date']
+        post['comments'].append(new_comment)
+        save_files(POSTS_FILE, posts)
+        return jsonify(new_comment), 201
+    else:
+        error_messages = []
+        if 'content' not in new_comment:
+            error_messages.append("Missing field: content")
+        if 'author' not in new_comment:
+            error_messages.append("Missing field: author")
+        error_message = {"error": ", ".join(error_messages)}
+        return jsonify(error_message), 400
+
+
+@app.route('/api/posts/<int:id>/comments', methods=['GET'])
+def get_comments(id):
+    post = find_post_by_id(id)
+    if post is None:
+        return jsonify({"error": "Post was not found"}), 404
+    return jsonify(post['comments'])
+
+
+@app.route('/api/posts/<int:id>/categories', methods=['POST'])
+@jwt_required()
+def add_category(id):
+    posts = open_files(POSTS_FILE)
+    post = find_post_by_id(id)
+    if post is None:
+        return jsonify({"error": "Post was not found"}), 404
+    new_category = request.get_json()
+    if 'category' in new_category:
+        post['categories'].append(new_category['category'])
+        save_files(new_category)
+        return jsonify(new_category), 201
+    else:
+        return jsonify({"error": "Missing field: category"}), 400
+
+
+@app.route('/api/posts/<int:id>/categories', methods=['GET'])
+def get_categories(id):
+    post = find_post_by_id(id)
+    if post is None:
+        return jsonify({"error": "Post was not found"}), 404
+    return jsonify(post['categories'])
+
+
+@app.route('/api/posts/<int:id>/tags', methods=['POST'])
+@jwt_required()
+def add_tag(id):
+    posts = open_files(POSTS_FILE)
+    post = find_post_by_id(id)
+    if post is None:
+        return jsonify({"error": "Post was not found"}), 404
+    new_tag = request.get_json()
+    if 'tags' in new_tag:
+        post['tags'].append(new_tag['tags'])
+        save_files(POSTS_FILE, posts)
+        return jsonify(new_tag), 201
+    else:
+        return jsonify({"error": "Missing field: tags"})
+
+
+@app.route('/api/posts/<int:id>/tags', methods=['GET'])
+def get_tags(id):
+    post = find_post_by_id(id)
+    if post is None:
+        return jsonify({"error": "Post was not found"}), 404
+    return jsonify(post['tags'])
 
 
 if __name__ == '__main__':
--- frontend/static/main.js
+++ frontend/static/main.js
@@ -5,18 +5,67 @@ window.onload = function() {
     // If a base URL is found in local storage, load the posts
     if (savedBaseUrl) {
         document.getElementById('api-base-url').value = savedBaseUrl;
-        loadPosts();
+    loadPosts();
     }
+}
+
+function register() {
+    var baseUrl = document.getElementById('api-base-url').value;
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Send a POST request to the registration endpoint
+    fetch(baseUrl + '/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username: username, password: password })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Registration successful');
+        // Optional: You can automatically log in the user after registration if desired
+        // Call the login function here or display a success message to the user
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+
+function login() {
+    var baseUrl = document.getElementById('api-base-url').value;
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Send a POST request to the login endpoint
+    fetch(baseUrl + '/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username: username, password: password })
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Store the access token in localStorage
+        localStorage.setItem('accessToken', data.access_token);
+        console.log('Login successful');
+
+        // Call the function to load posts
+        loadPosts();
+    })
+    .catch(error => console.error('Error:', error));
 }
 
 // Function to fetch all the posts from the API and display them on the page
 function loadPosts() {
     // Retrieve the base URL from the input field and save it to local storage
     var baseUrl = document.getElementById('api-base-url').value;
+    var accessToken = localStorage.getItem('accessToken');
     localStorage.setItem('apiBaseUrl', baseUrl);
 
     // Use the Fetch API to send a GET request to the /posts endpoint
-    fetch(baseUrl + '/posts')
+    fetch(baseUrl + '/posts', {
+        headers: {
+            'Authorization': 'Bearer ' + accessToken
+        }
+    })
         .then(response => response.json())  // Parse the JSON data from the response
         .then(data => {  // Once the data is ready, we can use it
             // Clear out the post container first
@@ -28,7 +77,13 @@ function loadPosts() {
                 const postDiv = document.createElement('div');
                 postDiv.className = 'post';
                 postDiv.innerHTML = `<h2>${post.title}</h2><p>${post.content}</p>
-                <button onclick="deletePost(${post.id})">Delete</button>`;
+                <p>Author: ${post.author}</p>
+                <p>Date: ${post.date}</p>
+                  <div class="post-buttons">
+                    <button onclick="deletePost(${post.id})">Delete</button>
+                    <button onclick="updatePost(${post.id})">Update</button>
+                  </div>
+                `;
                 postContainer.appendChild(postDiv);
             });
         })
@@ -39,14 +94,20 @@ function loadPosts() {
 function addPost() {
     // Retrieve the values from the input fields
     var baseUrl = document.getElementById('api-base-url').value;
+    var accessToken = localStorage.getItem('accessToken');
     var postTitle = document.getElementById('post-title').value;
     var postContent = document.getElementById('post-content').value;
+    var currentDate = new Date();
+    var formattedDate = currentDate.toLocaleDateString();
 
     // Use the Fetch API to send a POST request to the /posts endpoint
     fetch(baseUrl + '/posts', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ title: postTitle, content: postContent })
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken
+        },
+        body: JSON.stringify({ title: postTitle, content: postContent, date: formattedDate })
     })
     .then(response => response.json())  // Parse the JSON data from the response
     .then(post => {
@@ -59,14 +120,82 @@ function addPost() {
 // Function to send a DELETE request to the API to delete a post
 function deletePost(postId) {
     var baseUrl = document.getElementById('api-base-url').value;
+    var accessToken = localStorage.getItem('accessToken');
 
     // Use the Fetch API to send a DELETE request to the specific post's endpoint
     fetch(baseUrl + '/posts/' + postId, {
-        method: 'DELETE'
+        method: 'DELETE',
+        headers: {
+            'Authorization': 'Bearer ' + accessToken
+        }
     })
     .then(response => {
         console.log('Post deleted:', postId);
         loadPosts(); // Reload the posts after deleting one
     })
     .catch(error => console.error('Error:', error));  // If an error occurs, log it to the console
+}
+function updateBaseUrl() {
+    var baseUrl = document.getElementById('api-base-url').value;
+    localStorage.setItem('apiBaseUrl', baseUrl);
+    loadPosts();
+}
+
+// Function to search for posts based on filters
+function searchPosts() {
+    var baseUrl = document.getElementById('api-base-url').value;
+    var accessToken = localStorage.getItem('accessToken');
+    var title = document.getElementById('search-title').value;
+    var content = document.getElementById('search-content').value;
+    var author = document.getElementById('search-author').value;
+    var date = document.getElementById('search-date').value;
+
+    var url = new URL(baseUrl + '/posts/search');
+    if (title) url.searchParams.append('title', title);
+    if (content) url.searchParams.append('content', content);
+    if (author) url.searchParams.append('author', author);
+    if (date) url.searchParams.append('date', date);
+
+    fetch(url, {
+        headers: {
+            'Authorization': 'Bearer ' + accessToken
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        const postContainer = document.getElementById('post-container');
+        postContainer.innerHTML = '';
+
+        data.forEach(post => {
+            const postDiv = document.createElement('div');
+            postDiv.className = 'post';
+            postDiv.innerHTML = `<h2>${post.title}</h2><p>${post.content}</p>
+            <button onclick="deletePost(${post.id})">Delete</button>`;
+            postContainer.appendChild(postDiv);
+        });
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+// Function to update a post
+function updatePost(postId) {
+    var baseUrl = document.getElementById('api-base-url').value;
+    var accessToken = localStorage.getItem('accessToken');
+    var postTitle = document.getElementById('update-title').value;
+    var postContent = document.getElementById('update-content').value;
+
+    fetch(baseUrl + '/posts/' + postId, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken
+        },
+        body: JSON.stringify({ title: postTitle, content: postContent })
+    })
+    .then(response => response.json())
+    .then(updatedPost => {
+        console.log('Post updated:', updatedPost);
+        loadPosts();
+    })
+    .catch(error => console.error('Error:', error));
 }
